fed1f5ecfb811919ff5658335ea0cde42eb2696b
hernanf10 <141663927+hernanf10@users.noreply.github.com>
2024-07-07 23:24:43 -0300

    Update reservations.js
    ultima modificacion

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -1,32 +1,58 @@
-class Customer {}
+class Customer {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    get info() {
+        return `${this.name} - ${this.email}`;//
+    }
+}
+class Reservation {
+    constructor(id, customer, date, guests) {         //
+        this.id = id;
+        this.customer = customer;
+        this.date = new Date(date);
+        this.guests = guests;
+    }
+    get info() {
+        return `Fecha y hora: ${this.date.toLocaleString()}, Cliente: ${this.customer.info}, Número de comensales: ${this.guests}`;//
+    }
+    static validateReservation(reservationInfo) {
+        const { date, guests } = reservationInfo;//
+        const currentDate = new Date();
 
-class Reservation {}
+        if (new Date(date) < currentDate || guests <= 0) {
+            alert("Reserva no válida. Revisar fecha y el número de comensales.");		
+            return false;
+        }
 
+        return true;
+    }
+}
 class Restaurant {
     constructor(name) {
         this.name = name;
         this.reservations = [];
     }
-
     addReservation(reservation) {
-        this.reservations.push(reservation);
+        this.reservations.push(reservation);//
     }
-
     render() {
-        const container = document.getElementById("reservations-list");
+        const container = document.getElementById("reservations-list");			
         container.innerHTML = "";
         this.reservations.forEach((reservation) => {
             const reservationCard = document.createElement("div");
             reservationCard.className = "box";
             reservationCard.innerHTML = `
-                    <p class="subtitle has-text-primary">
+                    <p class="subtitle has-text-primary">			
                         Reserva ${
                             reservation.id
                         } - ${reservation.date.toLocaleString()}
                     </p>
                     <div class="card-content">
                         <div class="content">
-                            <p>
+                            <p>			
                                 ${reservation.info}
                             </p>
                         </div>
@@ -36,50 +62,25 @@ class Restaurant {
         });
     }
 }
-
-document
-    .getElementById("reservation-form")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const customerName = document.getElementById("customer-name").value;
-        const customerEmail = document.getElementById("customer-email").value;
-        const reservationDate =
-            document.getElementById("reservation-date").value;
-        const guests = parseInt(document.getElementById("guests").value);
-
-        if (Reservation.validateReservation(reservationDate, guests)) {
-            const customerId = restaurant.reservations.length + 1;
-            const reservationId = restaurant.reservations.length + 1;
-
-            const customer = new Customer(
-                customerId,
-                customerName,
-                customerEmail
-            );
-            const reservation = new Reservation(
-                reservationId,
-                customer,
-                reservationDate,
-                guests
-            );
-
-            restaurant.addReservation(reservation);
-            restaurant.render();
-        } else {
-            alert("Datos de reserva inválidos");
-            return;
-        }
-    });
-
 const restaurant = new Restaurant("El Lojal Kolinar");
+document.getElementById("reservation-form").addEventListener("submit", function (event) {			
+    event.preventDefault();
 
-const customer1 = new Customer(1, "Shallan Davar", "shallan@gmail.com");
-const reservation1 = new Reservation(1, customer1, "2024-12-31T20:00:00", 4);
+    const customerName = document.getElementById("customer-name").value;
+    const customerEmail = document.getElementById("customer-email").value;//
+    const reservationDate = document.getElementById("reservation-date").value;		
+    const guests = parseInt(document.getElementById("guests").value);
 
-if (Reservation.validateReservation(reservation1.date, reservation1.guests)) {
-    restaurant.addReservation(reservation1);
-    restaurant.render();
-} else {
-    alert("Datos de reserva inválidos");
-}
+    const reservationInfo = { date: reservationDate, guests };
+
+    if (Reservation.validateReservation(reservationInfo)) {
+        const customerId = restaurant.reservations.length + 1;		
+        const reservationId = restaurant.reservations.length + 1;
+
+        const customer = new Customer(customerId, customerName, customerEmail);
+        const reservation = new Reservation(reservationId, customer, reservationDate, guests);
+
+        restaurant.addReservation(reservation);
+        restaurant.render();
+    }
+});
